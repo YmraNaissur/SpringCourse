d0a4c30a816a490c1a18d38a18f2f43db2a93edd
YmraNaissur <daily.spb@yandex.ru>
2017-07-28 18:42:56 +0300

    Три новых поля, новые конструкторы

--- src/main/java/springRobot/robot/ModelT1000.java
+++ src/main/java/springRobot/robot/ModelT1000.java
@@ -10,13 +10,18 @@ import springRobot.interfaces.Robot;
  * 26.07.2017
  */
 public class ModelT1000 implements Robot {
-    private Hand hand;
-    private Head head;
-    private Leg leg;
+    private Hand hand;  // руки
+    private Head head;  // голова
+    private Leg leg;    // ноги
+
+    private String color;   // цвет робота
+    private int year;       // год выпуска
+    private boolean soundEnabled;   // наличие звука
 
     // Конструктор по умолчанию
     public ModelT1000() {    }
 
+    // Конструктор с тремя параметрами для указания запчастей
     public ModelT1000(Hand hand, Head head, Leg leg) {
         super();
         this.hand = hand;
@@ -24,13 +29,89 @@ public class ModelT1000 implements Robot {
         this.leg = leg;
     }
 
+    // Конструктор с тремя параметрами для указания свойств
+    public ModelT1000(String color, int year, boolean soundEnabled) {
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    // Конструктор со всеми параметрами
+    public ModelT1000(Hand hand, Head head, Leg leg, String color, int year, boolean soundEnabled) {
+        super();
+        this.hand = hand;
+        this.head = head;
+        this.leg = leg;
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    // Выполняем какие-то действия
+    // Метод с внедрением зависимостей
     public void action() {
         head.calc();
         hand.catchSomething();
         leg.go();
     }
 
+    // Танцуем
+    // Метод без внедрения зависимостей
     public void dance() {
         System.out.println("T1000 is dancing");
+    }
+
+    // Выводим информацию о параметрах робота
+    public void checkParameters() {
+        System.out.printf("Color: %s%nYear: %d%nSound enabled: %b%n", color, year, soundEnabled);
+    }
+
+    // Геттеры и сеттеры
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
     }
 }
